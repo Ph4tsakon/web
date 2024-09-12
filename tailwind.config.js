13e3bb613a3f9cd9d968ab264535ebbf4929d275
Ph4tsakon <phatsakon.pa.66@ubu.ac.th>
2024-09-12 10:59:10 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media",
+  content: ['./templates/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        angsana:["Angsana New"],
+        anthiti:["Anthiti"],
+        kanit:["Kanit"],
+      }
+    },
+  },
+  plugins: [],
+}
+
